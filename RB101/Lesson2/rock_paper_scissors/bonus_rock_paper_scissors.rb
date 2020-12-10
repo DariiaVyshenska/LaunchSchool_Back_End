6c105a64b37297e0e10649f6ef4f6ff557bb46c6
DariiaVyshenska <vyshenskaia@gmail.com>
2020-12-10 19:25:32 +0000

    updated assignment bonus RPS

--- RB101/Lesson2/rock_paper_scissors/bonus_rock_paper_scissors.rb
+++ RB101/Lesson2/rock_paper_scissors/bonus_rock_paper_scissors.rb
@@ -1,4 +1,5 @@
-ROUND_SIZE = 5
+require 'yaml'
+
 VALID_CHOICES = %w(r p sc sp l)
 WIN_RULES_HASH = {
   'r' => %w(l sc),
@@ -7,34 +8,53 @@ WIN_RULES_HASH = {
   'sp' => %w(sc r),
   'l' => %(sp p)
 }
-INTRO_MSG = <<-MSG
-  Enter one:
-    r for rock
-    p for paper
-    l for lizard
-    sc for scissors
-    sp for spock
-MSG
+LANGUAGE = 'en'
+MESSAGES = YAML.load_file("bonus_rps.yml")
 
-def prompt(message)
-  Kernel.puts "=> #{message}"
+#==============================METHODS SECTION==================================
+def clear
+  system('clear') || system('cls')
+end
+
+def messages(message_key, lang='en')
+  MESSAGES[lang][message_key]
+end
+
+def prompt(key, value='')
+  message = messages(key, LANGUAGE)
+  puts "=> #{message}" + value
+end
+
+def get_user_choice
+  choice = ''
+  loop do
+    prompt('ask_to_choose')
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break choice
+    else
+      prompt('not_valid_error')
+      prompt('instruction')
+    end
+  end
 end
 
 def win?(first, second)
   WIN_RULES_HASH[first].include?(second)
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("Computer won!")
+def who_won_battle?(user_choice, computer_choice)
+  if win?(user_choice, computer_choice)
+    'user'
+  elsif win?(computer_choice, user_choice)
+    'computer'
   else
-    prompt("It's a tie!")
+    'tie'
   end
 end
 
-def who_won?(score_hash)
+def who_won_round?(score_hash)
   if score_hash['you'] > score_hash['computer']
     'you'
   elsif score_hash['you'] < score_hash['computer']
@@ -44,59 +64,85 @@ def who_won?(score_hash)
   end
 end
 
+def update_score(score_hash, battle_winner)
+  case battle_winner
+  when 'user' then score_hash['you'] += 1
+  when 'computer' then score_hash['computer'] += 1
+  when 'tie' then nil
+  end
+end
+
+def display_choices(user_choice, computer_choice, battle_number)
+  clear
+  prompt('battle_number', battle_number.to_s)
+  prompt('user_choice', user_choice)
+  prompt('comp_choice', computer_choice)
+end
+
+def display_score(round_score)
+  prompt('small_break')
+  prompt('score')
+  prompt('player1', round_score['you'].to_s)
+  prompt('player2', round_score['computer'].to_s)
+  prompt('small_break')
+end
+
+def display_battle_results(battle_winner)
+  prompt(battle_winner)
+end
+
 def display_round_results(score_hash)
-  winner = who_won?(score_hash)
+  round_winner = who_won_round?(score_hash)
   round_message =
-    case winner
-    when 'you'
-      "You won this round!"
-    when 'computer'
-      "Computer won this round!"
-    when 'tie'
-      "It's a tie for this round!"
-    else
-      "Error: something went wrong!"
+    case round_winner
+    when 'you'      then 'round_user_won'
+    when 'computer' then 'round_comp_won'
+    else                 'general_error'
     end
+  prompt('star_break')
   prompt(round_message)
+  prompt('star_break')
 end
 
-def update_score(score_hash, player, computer)
-  if win?(player, computer)
-    score_hash['you'] += 1
-  elsif win?(computer, player)
-    score_hash['computer'] += 1
-  else
-    score_hash['tie'] += 1
-  end
-  nil
-end
-
-loop do
-  round_score = Hash.new(0)
-  ROUND_SIZE.times do
-    choice = ''
-    loop do
-      prompt(INTRO_MSG)
-      choice = Kernel.gets().chomp()
-
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("That's not a valid choice.")
-      end
+def play_again?
+  loop do
+    prompt 'play_again'
+    user_answer = gets.chomp.strip.downcase
+    case user_answer
+    when 'y' then break true
+    when 'n' then break false
+    else
+      prompt 'not_valid_error'
+      next
     end
-
-    computer_choice = VALID_CHOICES.sample
-
-    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-    display_results(choice, computer_choice)
-    update_score(round_score, choice, computer_choice)
   end
-
-  display_round_results(round_score)
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
 end
 
-prompt("Thank you for playing! Good bye!")
+def play_round
+  round_score = Hash.new(0)
+  battle = 0
+  loop do
+    prompt('instruction')
+    user_choice = get_user_choice
+    computer_choice = VALID_CHOICES.sample
+    winner = who_won_battle?(user_choice, computer_choice)
+    update_score(round_score, winner)
+    battle += 1
+
+    display_choices(user_choice, computer_choice, battle)
+    display_battle_results(winner)
+    display_score(round_score)
+    break if round_score.any? { |_, value| value == 5 }
+  end
+  display_round_results(round_score)
+end
+#============================END METHODS SECTION================================
+
+clear
+prompt 'welcome'
+loop do
+  play_round
+  break unless play_again?
+  clear
+end
+prompt('goodbye')
